ac6ff285d4a8814e254d779b5988399e116cac87
João Magalhães <joamag@gmail.com>
2025-01-19 02:04:18 +0000

    refactor: simplify wait_job method by removing unnecessary lines

--- src/colony_print/job.py
+++ src/colony_print/job.py
@@ -21,16 +21,13 @@ class JobAPI(object):
     def wait_job(self, id, sleep=0.5, iterations=20):
         for _ in range(iterations):
             time.sleep(sleep)
-
             try:
                 job = self.get_job(id)
             except Exception:
                 continue
-
             status = job.get("status", None)
             if not status == "finished":
                 continue
-
             if not "result" in job:
                 raise appier.OperationalError(message="No result found")
             return job["result"]
